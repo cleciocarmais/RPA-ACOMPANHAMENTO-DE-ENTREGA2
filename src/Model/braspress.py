d4c8cfd47eb8b040538e4c9f75cbd8a3da51e6ba
cleciocarmais <121456337+cleciocarmais@users.noreply.github.com>
2025-09-21 21:16:33 -0300

    criando funcao de aceeso da cbrix e braspress

---
+++ src/Model/braspress.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from dotenv import load_dotenv
+import pyautogui as p
+import os
+import logging
+
+
+
+
+def brasspress():
+    load_dotenv()
+    with open(os.getenv("CredenciaisBrasspress")) as file:
+        chaves = file.readlines()
+        usuario = chaves[0].strip()
+        senha = chaves[1].strip()
+    print("Acessando Site de Braspress")
+    logging.info("Acessando Site de Braspress")
+    navegador = webdriver.Chrome()
+    navegador.get("https://www.braspress.com/area-do-cliente/minha-conta/")
+  
+    p.sleep(2)
+    navegador.maximize_window()
+    p.sleep(1)
+    #ESPERANDO TELA DE COOCKIES APARECE:
+    # p.alert("teste")
+    try:
+        WebDriverWait(navegador, 20).until(ec.visibility_of_element_located((By.XPATH, "/html/body/footer/div[6]/div[3]/div")))
+        p.sleep(1)
+        navegador.find_element(By.XPATH, "/html/body/footer/div[6]/div[3]/div/div[3]/button").click()
+        p.sleep(2)
+    except:
+        pass
+    p.sleep(2)
+    navegador.switch_to.frame(navegador.find_element(By.XPATH, "/html/body/main/div/div/div/iframe"))
+    WebDriverWait(navegador, 20).until(ec.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div/div/div[1]/div/form/div[2]/div/div[1]/div/input")))
+    p.sleep(2)
+    #CAMPO USUARIO
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div[1]/div/form/div[2]/div/div[1]/div/input").send_keys(usuario, )
+    p.sleep(2)
+    #CAMPO SENHA
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div[1]/div/form/div[2]/div/div[2]/div/input").send_keys(senha, )
+    p.sleep(2)
+    #BTN ENTRAR
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div[1]/div/form/div[2]/div/div[3]/input").click()
+    p.sleep(2)
+    #PAGINA PRINCIPAL
+    WebDriverWait(navegador, 15).until(ec.visibility_of_element_located((By.XPATH, "/html/body/main/div/div[2]/div/div[2]/div[4]/button")))
+    #BTN DE ENCOMENDADS
+    navegador.find_element(By.XPATH, "/html/body/main/div/div[2]/div/div[2]/div[4]/button").click()
+    p.sleep(5)
+    #CAMPO DATA INICIAL
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/form/div/div/div/div[1]/div[1]/div/input").send_keys("01/09/2025", Keys.ENTER)
+    p.sleep(2)
+    #CAMPO DATA FINAL
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/form/div/div/div/div[1]/div[2]/div/input").send_keys("19/09/2025",Keys.ENTER)
+    p.sleep(2)
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/form/div/div/div/div[7]/div/label[1]").click()
+    p.alert(2)
+
+if __name__ == "__main__":
+    brasspress()
--- src/Model/bridex.py
+++ src/Model/bridex.py
@@ -6,8 +6,9 @@ from selenium.webdriver.support import expected_conditions as ec
 from dotenv import load_dotenv
 import pyautogui as p
 import os
+import logging
 
-def transportadora_bridex():
+def transportadora_bridex(codigo):
     load_dotenv()
     #Abrindo arquivo de credencias:
     with open(os.getenv("CredenciaisCBirdex")) as file:
@@ -28,8 +29,28 @@ def transportadora_bridex():
     p.sleep(5)
     #Campo input senha
     navegador.find_element(By.XPATH, "/html/body/div[1]/main/div/div/section/div/form/div[2]/div/input").send_keys(senha, Keys.ENTER)
-    p.sleep(1)
-    p.alert("ola")
+    #Buttom de rastreamento
+    WebDriverWait(navegador, 15).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div[1]/button")))
+    p.sleep(3)
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[1]/button").click()
+    p.sleep(5)
+    #BTN REMETENTE
+    navegador.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[1]/ul/li[1]/a").click()
+    p.sleep(5)
+    #CAMPO PESQUISA
+    navegador.find_element(By.XPATH, "/html/body/div[1]/main/div/div/div/div/section[1]/div[1]/div[1]/div/input").send_keys(codigo, Keys.ENTER)
+    p.sleep(4)
+    #Verficar se ouve algum retorno da pesquisa
 
+    listas = navegador.find_elements(By.XPATH, "/html/body/div[1]/main/div/div/div/div/section[2]/div/div[2]/a")
+    if len(listas) > 0:
+        for x in listas:
+            print(x.text)
+        
+    else:
+        print("Objeto nao coletado")
+        logging.info("Objeto nao coletado")
+        return "Nao Coletado"
+    p.alert("ola")
 if __name__ == "__main__":
-    transportadora_bridex()
+    transportadora_bridex("00148724")
